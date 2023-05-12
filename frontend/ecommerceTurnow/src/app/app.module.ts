74a6cff6852a832462704d334d3a4c525eaa5be8
solefanjbj <solefanjbj@gmail.com>
2023-05-11 22:30:07 -0300

    se agregaron componentes faltantes

--- frontend/ecommerceTurnow/src/app/app.module.ts
+++ frontend/ecommerceTurnow/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { FooterComponent } from './component/footer/footer.component';
 import { LoginComponent } from './component/login/login.component';
 import { CarritoComponent } from './component/carrito/carrito.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './component/contacto/contacto.component';
+import { NosotrosComponent } from './component/nosotros/nosotros.component';
 
 
 const appRoutes: Routes = [
@@ -29,7 +31,9 @@ const appRoutes: Routes = [
     HeaderComponent,
     FooterComponent,
     LoginComponent,
-    CarritoComponent
+    CarritoComponent,
+    ContactoComponent,
+    NosotrosComponent
      ],
   imports: [
     BrowserModule,
