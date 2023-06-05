3710e75e46c0a814a65c295ad323bb63d504bbb9
solefanjbj <solefanjbj@gmail.com>
2023-06-04 21:18:03 -0300

    Conexión desde el front con el back del login

--- frontend/ecommerceTurnow/src/app/component/login/login.component.ts
+++ frontend/ecommerceTurnow/src/app/component/login/login.component.ts
@@ -1,7 +1,8 @@
-import { NgFor } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormControl,  FormGroup,  NgForm, Validators } from '@angular/forms';
+import { FormControl, FormBuilder,  FormGroup,  NgForm, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { LoginService } from 'src/app/service/login.service';
+import { RequestStatus } from 'src/app/model/statusrequest';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +10,13 @@ import { LoginService } from 'src/app/service/login.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent  implements OnInit {
+
+  form = this.formBuilders.group({
+    correo: ['', [Validators.email, Validators.required, Validators.pattern('^\\w+([.-]?\\w+)*@\\w+([.-]?\\w+)*(\\.\\w{2,3})+$')
+  ]],
+    password: ['', Validators.required],
+  })
+  status: RequestStatus = 'init'
 
   get correo(){
     return this.formUser.get('correo') as FormControl;
@@ -28,7 +36,11 @@ export class LoginComponent  implements OnInit {
 
 
 
-  constructor() { 
+  constructor(
+    private loginService: LoginService,
+    private formBuilders: FormBuilder,
+    private router: Router
+  ) { 
 
   }
 
@@ -36,14 +48,29 @@ export class LoginComponent  implements OnInit {
 
    }
 
-  /*login(form: NgForm){
-
-    const email=form.value.email
-
-    const password=form.value.password
-
-  //  this.loginService.login(email, password);
-
-  }  */
+   login(e: Event) {
+    e.preventDefault()
+    if(this.form.valid){
+      this.status = 'loading'
+      const {correo, password} = this.form.getRawValue()
+      this.loginService.login(correo as string, password as string)
+      .subscribe({
+        next: (s) => {
+          this.status = 'success'
+          this.router.navigate(['/home'])
+        },
+        error: () => {
+          this.status = 'failed'
+          setTimeout(() => {
+            this.status = 'init'
+          }, 2000)
+          console.log('error')
+        }
+      })
+      console.log(this.form.value)
+    }else {
+      this.form.markAllAsTouched()
+    }
+   }
 
 }
